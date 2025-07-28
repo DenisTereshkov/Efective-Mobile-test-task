6e5dce5d9199373d8f1823a8b7e6213e1198bb32
Dennis Tereshkow <tereshkowdennis@gmail.com>
2025-07-28 19:40:09 +0300

    feat: add crud and profile templates

--- barter_platform/ads/urls.py
+++ barter_platform/ads/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.ads_list, name='ads_list'),
     path('new/', views.create_ad, name='create'),
-    path('<int:pk>/edit/', views.edit_ad, name='edit_ad'),
+    path('profile/', views.user_profile, name='profile'),
+    path('<int:pk>/', views.ad_detail, name='detail'),
+    path('<int:pk>/edit/', views.edit_ad, name='edit'),
+    path('<int:pk>/delete/', views.delete_ad, name='delete'),
 ]
--- barter_platform/ads/views.py
+++ barter_platform/ads/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from .models import Ad
 from .forms import AdForm
 
+@login_required
 def create_ad(request):
     if request.method == 'POST':
         form = AdForm(request.POST)
@@ -12,8 +16,9 @@ def create_ad(request):
             return redirect('ads_list')
     else:
         form = AdForm()
-    return render(request, 'ads/ad_form.html', {'form': form})
+    return render(request, 'ads/ad_create.html', {'form': form})
 
+@login_required
 def edit_ad(request, pk):
     ad = get_object_or_404(Ad, pk=pk)
     if request.method == 'POST':
@@ -28,3 +33,25 @@ def edit_ad(request, pk):
 def ads_list(request):
     ads = Ad.objects.all().order_by('-created_at')
     return render(request, 'ads/ads_list.html', {'ads': ads})
+
+def ad_detail(request, pk):
+    ad = get_object_or_404(Ad, pk=pk)
+    return render(request, 'ads/ad_detail.html', {'ad': ad})
+
+@login_required
+def delete_ad(request, pk):
+    ad = get_object_or_404(Ad, pk=pk)
+    if request.user != ad.user:
+        messages.error(request, 'Вы не можете удалить это объявление')
+        return redirect('ads_list')
+    if request.method == 'POST':
+        ad.delete()
+        messages.success(request, 'Объявление успешно удалено')
+        return redirect('profile')
+    return render(request, 'ads/confirm_delete.html', {'ad': ad})
+
+
+@login_required
+def user_profile(request):
+    user_ads = Ad.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'ads/profile.html', {'ads': user_ads})
